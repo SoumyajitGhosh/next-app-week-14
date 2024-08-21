25aebbf05eb2912f932c4b624a4ac5c9d9dbc9f1
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-08-21 22:17:12 +0530

    Learn:BE with Next, Data fetching, Singleton Prisma

--- next-app/app/(auth)/signup/page.tsx
+++ next-app/app/(auth)/signup/page.tsx
@@ -1,3 +1,5 @@
-export default function Signup() {
-  return <div>Hello World</div>;
+import { Signup } from "@/components/Signup";
+
+export default function () {
+  return <Signup />;
 }
--- next-app/components/Signup.tsx
+++ next-app/components/Signup.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { Button } from "./Button";
+import axios from "axios";
+import { ChangeEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function SigninComponent() {
-  function handler() {
-    console.log("Sign in");
+export function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  async function handler() {
+    const response = await axios.post("http://localhost:3000/api/user", {
+      username,
+      password,
+    });
+    router.push("/");
   }
+
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -15,14 +26,20 @@ export default function SigninComponent() {
         >
           <div>
             <div className="px-10">
-              <div className="text-3xl font-extrabold">Sign in</div>
+              <div className="text-3xl font-extrabold">Sign up</div>
             </div>
             <div className="pt-2">
               <LabelledInput
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
                 label="Username"
                 placeholder="harkirat@gmail.com"
               />
               <LabelledInput
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
                 label="Password"
                 type={"password"}
                 placeholder="123456"
@@ -42,19 +59,19 @@ export default function SigninComponent() {
   );
 }
 
-interface LabelledInputType {
-  label: string;
-  placeholder: string;
-  type?: string;
-}
-
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
+function LabelledInput({
+  label,
+  placeholder,
+  type,
+  onChange,
+}: LabelledInputType) {
   return (
     <div>
       <label className="block mb-2 text-sm text-black font-semibold pt-4">
         {label}
       </label>
       <input
+        onChange={onChange}
         type={type || "text"}
         id="first_name"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
@@ -63,4 +80,11 @@ function LabelledInput({ label, placeholder, type }: LabelledInputType) {
       />
     </div>
   );
+}
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  type?: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }
